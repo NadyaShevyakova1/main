8f3858df80b31ade372e417e35ec6ce5c694579c
Nadya Shevyakova <nadya-shevyakova@mail.ru>
2021-05-22 21:11:26 +0300

    added student name input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 int main() {
 
-	std::cout << "Hello world!\n";
+	char name[20];
+
+	cout << "Enter your name: ";
+
+	cin >> name;
+
+	cout << "Hello, " << name << "!\n";
 
 	system("pause");
 
